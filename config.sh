ccff8e1220e8e4397722ae83a19b7fb3920eaa7f
Jonathan Hardison <jmhardison@users.noreply.github.com>
2016-11-23 11:05:08 -0600

    Update config.sh
    updated to pull from master of acme_tiny to include august agreement change.

--- config.sh
+++ config.sh
@@ -11,4 +11,4 @@ signed_cert="$cert_root_dir/signed_cert.crt"
 ssl_cert="$cert_root_dir/ssl_cert.pem"
 le_intermediate_ca="$cert_root_dir/le_intermediate_ca.pem"
 
-acme_tiny_url="https://raw.githubusercontent.com/diafygi/acme-tiny/fcb7cd6f66e951eeae76ff2f48d8ad3e40da37ef/acme_tiny.py"
+acme_tiny_url="https://raw.githubusercontent.com/diafygi/acme-tiny/master/acme_tiny.py"
